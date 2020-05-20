2a7d763165f18f95334a078c6660a6f098167aad
Hemant Pawar <hpawar@dwavesys.com>
2020-05-20 21:43:30 +0000

    Updated formatting

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,16 +1,16 @@
-#    Copyright 2020 D-Wave Systems Inc.
-
-#    Licensed under the Apache License, Version 2.0 (the "License")
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-
-#        http: // www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Copyright 2019 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import subprocess
 import unittest
@@ -25,17 +25,17 @@ class IntegrationTests(unittest.TestCase):
         """ Verify contents of output """
         
         demo_file = os.path.join(project_dir, 'demo.py')
-        output=subprocess.check_output([sys.executable, demo_file])
-        output=str(output)
+        output = subprocess.check_output([sys.executable, demo_file])
+        output = str(output).upper()
         if os.getenv('DEBUG_OUTPUT'):
-            print("Example output \n"+ output)
+            print("Example output \n" + output)
 
         with self.subTest(msg="Verify if output contains 'Jobs and the start times of each task' \n"):
-            self.assertIn("Jobs and the start times of each task".upper(),output.upper())
+            self.assertIn("Jobs and the start times of each task".upper(), output)
         with self.subTest(msg="Verify if error string contains in output \n"):
-            self.assertNotIn("ERROR",output.upper())
+            self.assertNotIn("ERROR", output)
         with self.subTest(msg="Verify if warning string contains in output \n"):
-            self.assertNotIn("WARNING",output.upper())
+            self.assertNotIn("WARNING", output)
 
 
 if __name__ == '__main__':
